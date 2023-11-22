56ba258aeef39ca5317921f53d8e21da4eb60006
Dmitriy Semashko <semashko@fgoupsk.ru>
2023-11-22 14:28:37 +0300

    Создание БД mySQL для социальной сети

--- shop.py
+++ shop.py
@@ -1,14 +1,29 @@
 # подключаем SQLite
-import sqlite3
-from sqlite3 import Error
+import mysql.connector
+from mysql.connector import Error
 
-def create_connection(path):
+def create_connection(host_name, user_name, user_password):
     connection = None
     try:
-        connection = sqlite3.connect(path)
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
         print("Подключение к SQLite БД выполнено успешно")
     except Error as e:
         print(f"Ошибка '{e}' обнаружена")
     return connection
 
-connection = create_connection("C:\\Temp\\social.sqlite")
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("БД успешно создана")
+    except Error as e:
+        print(f"Ошибка '{e}' обнаружена")
+
+
+connection = create_connection("localhost", "root", "root")
+create_database_query = "CREATE DATABASE social"
+create_database(connection, create_database_query)
